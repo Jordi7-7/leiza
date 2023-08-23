09d562c1e9937e8be8a515198e8fe54a7f1e210b
Jordi7-7 <jordi.cahuas20@gmail.com>
2023-08-22 19:55:18 -0600

    commit poderoso insert

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import {PORT} from './config.js';
 import express from 'express';
 import mysql from 'mysql';
 import bodyParser from 'body-parser';
+import fs from fs;
 
 const app = express();
 
@@ -74,6 +75,49 @@ app.get('/buscar/:id_prod',(req,res)=>{
     const id = req.params.id_prod;
 
     conexion.query('SELECT * FROM PRODUCTOS WHERE ID_PROD = ?',[id],(error,results)=>{
+
+        if(error) throw error;  
+
+        console.log(results);
+        res.json(results);
+        
+
+    });
+});
+
+
+app.post('/insertar',(req,res)=>{
+
+    const id = req.body.ID;
+    const nombre = req.body.NOMBRE;
+    const cantidad = req.body.CANTIDAD;
+    const precio = req.body.PRECIO;
+    const descripcion = req.body.DESCRIPCION;
+    const categoria = req.body.CATEGORIA;
+    const genero = req.body.GENERO;
+
+    //Guardar la imagen
+    const imageBase64 = req.body.file; // La imagen en formato binario como base64
+  const imageName = id+".jpg"; // Nombre que deseas para la imagen guardada
+  const directoryPath = path.join('recursos', 'img', 'Catalogo');
+
+  // Decodificar la imagen base64 a formato binario
+  const imageBuffer = Buffer.from(imageBase64, 'base64');
+  const imagePath = path.join(directoryPath, imageName);
+
+  // Guardar la imagen en el directorio
+  fs.writeFile(imagePath, imageBuffer, (err) => {
+    if (err) {
+      console.error('Error al guardar la imagen:', err);
+      res.status(500).send('Error al guardar la imagen');
+    } else {
+      console.log('Imagen guardada exitosamente');
+      res.status(200).send('Imagen guardada exitosamente');
+    }
+  });
+    
+
+    conexion.query("INSERT INTO productos VALUES ('"+id+"', "+genero+", "+categoria+",'"+nombre+"', '"+descripcion+"', "+cantidad+","+precio+",'"+imagePath+")",(error,results)=>{
 
         if(error) throw error;  
 
